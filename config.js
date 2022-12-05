6723431837d94a13c3862d1b1b9cedad27638076
Jagjit singh <jagjitsingh.btcivil20@pec.edu.in>
2022-12-05 15:22:20 +0530

    Api changes

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 
 import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth'
-
+import 'firebase/compat/auth';
+import { REACT_APP_API_KEY, REACT_APP_APP_ID, REACT_APP_AUTH_DOMAIN, REACT_APP_MEASUREMENT_ID, REACT_APP_MESSAGING_SENDER_ID, REACT_APP_PROJECT_ID, REACT_APP_STORAGE_BUCKET } from "@env"
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAmgjT4bRAsAju-gYBlyeYM9YTVpoFBU6k",
-    authDomain: "learning-de268.firebaseapp.com",
-    projectId: "learning-de268",
-    storageBucket: "learning-de268.appspot.com",
-    messagingSenderId: "441673338172",
-    appId: "1:441673338172:web:9a224ecd111394f835a239",
-    measurementId: "G-J86GPXQWTZ"
+    apiKey: REACT_APP_API_KEY,
+    authDomain: REACT_APP_AUTH_DOMAIN,
+    projectId: REACT_APP_PROJECT_ID,
+    storageBucket: REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+    appId: REACT_APP_APP_ID,
+    measurementId: REACT_APP_MEASUREMENT_ID
 };
 let app;
 
